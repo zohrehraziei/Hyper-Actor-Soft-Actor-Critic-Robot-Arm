13ca32c14ceb09599b5f5613997e999e6d5d5c6f
Zohreh Raziei <31635558+zohrehraziei@users.noreply.github.com>
2020-05-01 00:37:04 -0400

    Create models.py

---
+++ src/rl_modules/models.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+"""
+the input x in both networks should be [o, g], where o is the observation and g is the goal.
+
+"""
+
+# define the actor network
+class actor(nn.Module):
+    def __init__(self, env_params):
+        super(actor, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.action_out = nn.Linear(128, env_params['action'])
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        actions = self.max_action * torch.tanh(self.action_out(x))
+
+        return actions
+    
+class actor_module_init(nn.Module):
+    def __init__(self, env_params):
+        super(actor, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.action_out = nn.Linear(128, env_params['action'])
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        actions = self.max_action * torch.tanh(self.action_out(x))
+
+        return actions
+
+class actor_module_mid(nn.Module):
+    def __init__(self, env_params):
+        super(actor, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['action'], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.action_out = nn.Linear(128, env_params['action'])
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        actions = self.max_action * torch.tanh(self.action_out(x))
+
+        return actions
+
+class critic(nn.Module):
+    def __init__(self, env_params):
+        super(critic, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'] + env_params['action'], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.q_out = nn.Linear(128, 1)
+
+    def forward(self, x, actions):
+        x = torch.cat([x, actions / self.max_action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        q_value = self.q_out(x)
+
+        return q_value
+
+class critic_module_init(nn.Module):
+    def __init__(self, env_params):
+        super(critic, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'] + env_params['action'], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.q_out = nn.Linear(128, 1)
+
+    def forward(self, x, actions):
+        x = torch.cat([x, actions / self.max_action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        q_value = self.q_out(x)
+
+        return q_value
+
+class critic_module_mid(nn.Module):
+    def __init__(self, env_params):
+        super(critic, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'] + env_params['action'], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.q_out = nn.Linear(128, 1)
+
+    def forward(self, x, actions):
+        x = torch.cat([x, actions / self.max_action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        q_value = self.q_out(x)
+
+        return q_value
